9521dc0b701d694b867cb9ddede624e9343572b2
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-13 08:54:41 -0500

    adding Transit Service class with links to api urls and getBuses method

---
+++ src/main/java/com/techtalentsouth/TransitApp/service/TransitService.java
@@ -0,0 +1,30 @@
+package com.techtalentsouth.TransitApp.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class TransitService {
+	@Value("${transit_url}")
+    public String transitUrl;
+	
+    @Value("${geocoding_url}")
+    public String geocodingUrl;
+	
+    @Value("${distance_url}")
+    public String distanceUrl;
+	
+    @Value("${google_api_key}")
+    public String googleApiKey;
+    
+    private List<Bus> getBuses(){
+        RestTemplate restTemplate = new RestTemplate();
+        Bus[] buses = restTemplate.getForObject(transitUrl, Bus[].class);
+        return Arrays.asList(buses);
+    }
+
+}
