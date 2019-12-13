92f5fd2919f0fe7bae48dbf7da6946064419fddd
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-13 09:26:16 -0500

    importing comparator on BusComparator class

--- src/main/java/com/techtalentsouth/TransitApp/model/BusComparator.java
+++ src/main/java/com/techtalentsouth/TransitApp/model/BusComparator.java
@@ -1,5 +1,7 @@
 package com.techtalentsouth.TransitApp.model;
 
+import java.util.Comparator;
+
 public class BusComparator implements Comparator<Bus> {
 	@Override
     public int compare(Bus o1, Bus o2) {
