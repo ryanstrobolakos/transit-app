166cd0031f8ba87059a62d8db26018c30feb69b5
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-13 09:21:59 -0500

    adding getNearbyBuses method

--- src/main/java/com/techtalentsouth/TransitApp/service/TransitService.java
+++ src/main/java/com/techtalentsouth/TransitApp/service/TransitService.java
@@ -1,13 +1,17 @@
 package com.techtalentsouth.TransitApp.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.parsing.Location;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.techtalentsouth.TransitApp.model.Bus;
+import com.techtalentsouth.TransitApp.model.BusRequest;
 
 @Service
 public class TransitService {
@@ -27,6 +31,28 @@ public class TransitService {
         RestTemplate restTemplate = new RestTemplate();
         Bus[] buses = restTemplate.getForObject(transitUrl, Bus[].class);
         return Arrays.asList(buses);
+    }
+    
+    public List<Bus> getNearbyBuses(BusRequest request){
+        List<Bus> allBuses = this.getBuses();
+        Location personLocation = this.getCoordinates(request.address + " " + request.city);
+        List<Bus> nearbyBuses = new ArrayList<>();
+        for(Bus bus : allBuses) {
+            Location busLocation = new Location();
+            busLocation.lat = bus.LATITUDE;
+            busLocation.lng = bus.LONGITUDE;
+            double latDistance = Double.parseDouble(busLocation.lat) - Double.parseDouble(personLocation.lat);
+            double lngDistance = Double.parseDouble(busLocation.lng) - Double.parseDouble(personLocation.lng);
+            if (Math.abs(latDistance) <= 0.02 && Math.abs(lngDistance) <= 0.02) {
+                double distance = getDistance(busLocation, personLocation);
+                if (distance <= 1) {
+                    bus.distance = (double) Math.round(distance * 100) / 100;
+                    nearbyBuses.add(bus);
+                }
+            }
+        }
+        Collections.sort(nearbyBuses, new BusComparator());
+        return nearbyBuses;
     }
 
 }
