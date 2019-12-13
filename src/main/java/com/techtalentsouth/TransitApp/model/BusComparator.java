4d83c2902eee3200a0f23b5328fe62950b03ee42
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-13 09:22:15 -0500

    adding BusComparator class

---
+++ src/main/java/com/techtalentsouth/TransitApp/model/BusComparator.java
@@ -0,0 +1,11 @@
+package com.techtalentsouth.TransitApp.model;
+
+public class BusComparator implements Comparator<Bus> {
+	@Override
+    public int compare(Bus o1, Bus o2) {
+        if (o1.distance < o2.distance) return -1;
+        if (o1.distance > o2.distance) return 1;
+        return 0;
+    }
+
+}
